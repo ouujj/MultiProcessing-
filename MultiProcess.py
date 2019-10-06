4defd033b66f14302a87a13ddb1321fabd78b8fa
Saranyu Ngaosri <39403998+ouujj@users.noreply.github.com>
2019-10-06 22:12:26 +0700

    Add files via upload

--- MultiProcess.py
+++ MultiProcess.py
@@ -2,33 +2,42 @@ from multiprocessing import Process ,Manager
 import datetime 
 import time
 import random
-manager = Manager()
-QA = manager.list()
+import sys
 
-def detect():
+def detect(QA):
   while True:
+    sys.stdout.flush()
     QA.append(random.randrange(2,10,1))
+    #sys.stdout.write("New detect = ",QA[-1])
     print("New detect = ",QA[-1])
+    sys.stdout.flush()
     time.sleep(1)
     #print(QA)
 
-def teakQA():
+def teakQA(QA):
   while True:
-    if(len(QA) > 0):
+    if(len(QA) > 0):  
+      sys.stdout.flush()
       now=datetime.datetime.now()
       print(QA[0],"Start @",'%02d:%02d.%d'%(now.minute,now.second,now.microsecond))
+      sys.stdout.flush()
+
       time.sleep(QA[0])
       now=datetime.datetime.now()
       print("Done @",'%02d:%02d.%d'%(now.minute,now.second,now.microsecond))
+      sys.stdout.flush()
+      
       del(QA[0])
       #print(QA)
-if __name__ == "__main__":
-    
-    proc1 = Process(target=detect)
-    proc2 = Process(target=teakQA)
 
+if __name__ == "__main__":
+    QA = Manager().list()
+    proc1 = Process(target=detect ,args=(QA,),)
+    proc2 = Process(target=teakQA,args=(QA,),)
     proc1.start()
     proc2.start()
     proc1.join()
     proc2.join()
+
+    
 
